a74dd0b39d98c944c7cb023db46c7197fdd7ccf7
Brightosman <brichiile@gmail.com>
2025-05-11 04:27:03 +0200

    0.0.004

--- components/header/page.tsx
+++ components/header/page.tsx
@@ -127,7 +127,7 @@ export default  function Header() {
 
   // submenu handler
   const [openIndex, setOpenIndex] = useState(-1);
-  const handleSubmenu = (index) => {
+  const handleSubmenu = (index: number) => {
     if (openIndex === index) {
       setOpenIndex(-1);
     } else {
